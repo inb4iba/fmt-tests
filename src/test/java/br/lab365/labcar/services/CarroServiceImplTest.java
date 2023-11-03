404b1c79d96c6936fc5530616bfbc9eb2a5aa891
Icaro <iba1987@gmail.com>
2023-11-03 13:09:30 -0300

    exercise 4

--- src/test/java/br/lab365/labcar/services/CarroServiceImplTest.java
+++ src/test/java/br/lab365/labcar/services/CarroServiceImplTest.java
@@ -2,12 +2,16 @@ package br.lab365.labcar.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -37,20 +41,28 @@ public class CarroServiceImplTest {
         for (int i = 0; i < quantidade; i++)
             data.add(CarroFixture.criarCarro());
         when(repository.findAll()).thenReturn(data);
-        assertEquals(quantidade, repository.findAll().size());
+        assertEquals(quantidade, service.buscarTodos().size());
     }
 
     @Test
     void shouldReturnCarroById() {
         Long id = faker.number().randomNumber();
         when(repository.findById(id)).thenReturn(Optional.of(CarroFixture.criarCarro()));
-        assertNotNull(repository.findById(id));
+        assertNotNull(service.buscarPorId(id));
     }
 
     @Test
-    void shouldSaveCarro() {
+    void shouldSaveCarro() throws Exception {
         when(repository.save(any())).thenReturn(CarroFixture.criarCarro());
-        CarroModel carro = repository.save(CarroFixture.criarCarro());
+        CarroModel carro = service.salvar(CarroFixture.criarCarro());
         assertNotNull(carro);
+    }
+
+    @Nested
+    class Exceptions {
+        @Test
+        void shouldThrowNoSuchElementExceptionIfInvalidId() {
+            assertThrowsExactly(NoSuchElementException.class, () -> service.apagar(null));
+        }
     }
 }
